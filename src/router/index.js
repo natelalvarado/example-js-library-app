0f01e521a51c51570d734fc068db1888804d988e
Nate <natelalvarado@gmail.com>
2021-10-15 11:01:07 -0400

    Adds xkcd chart js library

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import xkcd from "../views/xkcd.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+
+  {
+    path: "/xkcd",
+    name: "xkcd",
+    component: xkcd,
   },
 ];
 
